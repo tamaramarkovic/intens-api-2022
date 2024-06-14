367b8e20b4d59cdbe8b5759aa268e972eed0e24b
Tamara <tmtamaramarkovic@gmail.com>
2024-06-14 18:45:28 +0200

    Exception is added

--- src/test/java/intens/api/praksa2022/Praksa2022ApplicationTests.java
+++ src/test/java/intens/api/praksa2022/Praksa2022ApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class Praksa2022ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		throw new Exception("Fail");
 	}
 
 }
